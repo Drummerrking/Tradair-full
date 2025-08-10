fd7218dea4e15e285c42c2fdcc5434de916efa39
Drummerrking <renobethell@gmail.com>
2025-08-10 12:45:50 +0100

    add firebase init

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,1 +1,16 @@
+// src/lib/firebase.js
+import { initializeApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyA9bTUaqs_TF0iG0iu9t5ZAdRGUMS_t0bY",
+  authDomain: "tradair-b2b8e.firebaseapp.com",
+  projectId: "tradair-b2b8e",
+  storageBucket: "tradair-b2b8e.firebasestorage.app",
+  messagingSenderId: "190146660959",
+  appId: "1:190146660959:web:5745b6d46970221725a9ea",
+};
+
+const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
