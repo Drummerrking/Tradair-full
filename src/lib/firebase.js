d49debb390d6ddd372dfb5ad6f940ea84474eaaf
Drummerrking <renobethell@gmail.com>
2025-08-11 13:21:25 +0100

    Add files via upload

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,16 +1,20 @@
 // src/lib/firebase.js
-import { initializeApp, getApps } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9bTUaqs_TF0iG0iu9t5ZAdRGUMS_t0bY",
-  authDomain: "tradair-b2b8e.firebaseapp.com",
-  projectId: "tradair-b2b8e",
-  storageBucket: "tradair-b2b8e.firebasestorage.app",
-  messagingSenderId: "190146660959",
-  appId: "1:190146660959:web:5745b6d46970221725a9ea",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export default app;
